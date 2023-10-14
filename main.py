070a3e8482f1907f3fc397f39d5339e0126c7f1c
Edimar-Matos <edimar.matos77@gmail.com>
2023-10-14 19:47:02 -0300

    sistema de assistente
    implantar vosk e pyaudio

---
+++ main.py
@@ -0,0 +1,47 @@
+'''
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+
+    while True:
+        audio = r.listen(source) # Define microphone como fonte de audio
+
+        print(r.recognize_google(audio, language='pt'))
+
+        '''
+
+
+#!/usr/bin/env python3
+
+# prerequisites: as described in https://alphacephei.com/vosk/install and also python module `sounddevice` (simply run command `pip install sounddevice`)
+# Example usage using Dutch (nl) recognition model: `python test_microphone.py -m nl`
+# For more help run: `python test_microphone.py -h`
+
+
+
+from vosk import Model, KaldiRecognizer
+
+import os
+
+import pyaudio
+
+
+model = Model("model")
+rec = KaldiRecognizer(model, 16000)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
+print(rec.FinalResult())        
+        
---
+++ teste/tts.py
@@ -0,0 +1,8 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("ola adriel")
+engine.say("Você gosta de falar inglês... eu também.")
+engine.say("Traduz essa, what is the name o your father")
+engine.say(" ..., ... edimar")
+
+engine.runAndWait()
