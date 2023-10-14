f8e0314de6e2cf95303de9e9608e3c427f62e896
Edimar-Matos <edimar.matos77@gmail.com>
2023-10-14 20:37:50 -0300

    Update tts.py
    ajustes

--- teste/tts.py
+++ teste/tts.py
@@ -1,8 +1,5 @@
 import pyttsx3
 engine = pyttsx3.init()
-engine.say("ola adriel")
-engine.say("Você gosta de falar inglês... eu também.")
-engine.say("Traduz essa, what is the name o your father")
-engine.say(" ..., ... edimar")
+engine.say("Esse é um teste de complementação")
 
 engine.runAndWait()
