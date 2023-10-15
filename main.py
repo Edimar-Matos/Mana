5e85746de38319adc2ed6ade2e2d0f0a60e046dd
Edimar-Matos <edimar.matos77@gmail.com>
2023-10-14 23:20:08 -0300

    assistente atual
    melhorias

--- main.py
+++ main.py
@@ -1,50 +1,20 @@
-'''
-import speech_recognition as sr
-
-r = sr.Recognizer()
-
-with sr.Microphone() as source:
-
-    while True:
-        audio = r.listen(source) # Define microphone como fonte de audio
-
-        print(r.recognize_google(audio, language='pt'))
-
-        '''
-
-
-#!/usr/bin/env python3
-
-# prerequisites: as described in https://alphacephei.com/vosk/install and also python module `sounddevice` (simply run command `pip install sounddevice`)
-# Example usage using Dutch (nl) recognition model: `python test_microphone.py -m nl`
-# For more help run: `python test_microphone.py -h`
-
-
-
 from vosk import Model, KaldiRecognizer
-
 import os
-
 import pyaudio
-
 import pyttsx3
-
 import json
 
-# sintese de fala
+# Síntese de fala
 engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
-
 engine.setProperty('voice', voices[-2].id)
 
-
-def speek(text):
+def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-
-model = Model("model")
+model = Model('model')
 rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
@@ -56,16 +26,11 @@ while True:
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-       result = rec.Result()
-       result = json.loads(result)
+        result = rec.Result()
+        result = json.loads(result)
 
-       if result is not None:
-           text = result['text']
+        if result is not None:
+            text = result['text']
 
-           print(text)
-           speek(text)
-
-
-       
-        
-        
+            print(text)
+            speak(text)
